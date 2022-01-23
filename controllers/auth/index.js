5f944d8c406c1ea7cff24ebd36d9ec0f81efa6bf
shaakilahammed <shaakilahammed@gmail.com>
2022-01-23 22:03:57 +0600

    Login Register API added

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,15 +1,40 @@
 const User = require('../../models/User');
+const CryptoJS = require('crypto-js');
 
 exports.register = async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC),
   });
 
   try {
-    const savedUser = newUser.save();
-    res.status(201).json(savedUser);
+    const savedUser = await newUser.save();
+    res
+      .status(201)
+      .json({ message: 'Successfully Registered', data: savedUser });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json('Wrong Username');
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json('Wrong Password');
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
   } catch (error) {
     res.status(500).json(error);
   }
--- routes/auth.js
+++ routes/auth.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const authController = require('../controllers/auth/index');
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
