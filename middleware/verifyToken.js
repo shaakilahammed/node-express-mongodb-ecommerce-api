736db53deb3a84c84595fba1471bbf8980d2c00d
shaakilahammed <shaakilahammed@gmail.com>
2022-01-24 22:12:28 +0600

    Authenticate with jwtwebtoken

---
+++ middleware/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json('Invalid Token');
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json('Unauthorized');
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    console.log(req.user);
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json('You are not allowed!');
+    }
+  });
+};
+module.exports = { verifyToken, verifyTokenAndAuthorization };
